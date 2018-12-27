39e861518ac57d7456652201d3ec4fa66511c233
flevanti <frenke@frenke.it>
2018-12-27 13:07:59 +0000

    fixing

--- main.go
+++ main.go
@@ -4,35 +4,42 @@ import (
 	"context"
 	"errors"
 	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/mongodb/mongo-go-driver/x/bsonx"
 )
 
-var mongoClient *mongo.Client
-var mongoDb *mongo.Database
-var mongoCollectionsList = make(map[string]bool)
+type connType struct {
+	client *mongo.Client
+	db     *mongo.Database
+}
 
 // initialiseMongo
-func Connect(uri string, db string) (bool, error) {
+func Connect(uri string, db string) (*connType, error) {
+	//var mongoClient *mongo.Client
+	//var mongoDb *mongo.Database
+	var newConn = new(connType);
 	if uri == "" {
-		return false, errors.New("uri not provided")
+		return newConn, errors.New("uri not provided")
 	}
 	var err error
 	// CREATE CLIENT
-	mongoClient, err = mongo.Connect(context.Background(), uri, nil)
+	newConn.client, err = mongo.Connect(context.Background(), uri, nil)
 	if err != nil {
-		return false, errors.New("Mongo db client creation failed: " + err.Error())
+		return newConn, errors.New("Mongo db client creation failed: " + err.Error())
 	}
-    defer closeMongo()
 
 	// SELECT DATABASE
-	dbExists, err := databaseExists(db)
-	if err != nil {
-		return false, errors.New("Error while retrieving databases list 💥: " + err.Error())
-	}
-	if !dbExists {
-		return false, errors.New("provided mongo db does not exists")
-	}
-	mongoDb = mongoClient.Database(db)
+	//dbExists, err := databaseExists(db)
+	//if err != nil {
+	//	return false, errors.New("Error while retrieving databases list 💥: " + err.Error())
+	//}
+	//if !dbExists {
+	//	return false, errors.New("provided mongo db does not exists")
+	//}
+	//newConn.client.Connect(context.Background());
+	newConn.db = newConn.client.Database(db)
+	cur, err := newConn.db.ListCollections(context.Background(), nil)
 
+	cur = cur
 	// RETRIEVE COLLECTIONS LIST
 	// TODO fix the issue around the bson newdocument .... driver has changed since last time....FFS
 	//err = retrieveCollectionsList()
@@ -40,59 +47,63 @@ func Connect(uri string, db string) (bool, error) {
 	//	return false, errors.New("Mongo db collections list not retrieved 💥:" + err.Error())
 	//}
 
-	return true, nil
+	return newConn, nil
 }
 
-//func retrieveCollectionsList() error {
-//	var cur mongo.Cursor
+func (conn connType) RetrieveCollectionsList() ([]string, error) {
+	var cur mongo.Cursor
+	var err error
+	var mongoCollectionsList []string
+
+	cnt := context.Background()
+
+	cur, err = conn.db.ListCollections(context.Background(), nil)
+	if err != nil {
+		return nil, errors.New("Mongo db collections list not retrieved: " + err.Error())
+	}
+
+	for cur.Next(cnt) {
+		elem := &bsonx.Doc{}
+		err := cur.Decode(elem)
+		if err != nil {
+			return nil, errors.New("Unable to decode element while reading collections list: " + err.Error())
+		}
+		name := elem.Lookup("name").StringValue()
+		mongoCollectionsList = append(mongoCollectionsList, name)
+	}
+
+	if err := cur.Err(); err != nil {
+		return nil, errors.New("Cursor error while reading collections list: " + err.Error())
+	}
+
+	return mongoCollectionsList, nil
+}
+
+//func databaseExists(databaseName string) (bool, error) {
+//	var databasesList []string
+//	//var databasesList []string
 //	var err error
-//	cnt := context.Background()
-//
-//	cur, err = mongoDb.ListCollections(context.Background(), nil)
+//	databasesList, err = mongoClient.ListDatabaseNames(context.Background(), nil)
+//	databasesList = databasesList
 //	if err != nil {
-//		return errors.New("Mongo db collections list not retrieved: " + err.Error())
+//		return false, err
 //	}
-//
-//	for cur.Next(cnt) {
-//		elem := bson.NewDocument()
-//		if err := cur.Decode(elem); err != nil {
-//			return errors.New("Unable to decode element while reading collections list: " + err.Error())
-//		}
-//		name :=  elem.Lookup("name").StringValue()
-//		mongoCollectionsList[name] = true
-//	}
-//
-//	if err := cur.Err(); err != nil {
-//		return errors.New("Cursor error while reading collections list: " + err.Error())
-//	}
-//
-//	return nil
+//	//for _, v := range databasesList2.Databases {
+//	//	if v == databaseName {
+//	//		return true, nil
+//	//	}
+//	//}
+//	return false, nil
 //}
 
-func databaseExists(databaseName string) (bool, error) {
-	var databasesList []string
-	var err error
+//func collectionExists(collectionName string) bool {
+//	_, exists := mongoCollectionsList[collectionName]
+//	return exists
+//}
 
-	databasesList, err = mongoClient.ListDatabaseNames(context.Background(), nil)
-	if err != nil {
-		return false, err
-	}
-	for _, v := range databasesList {
-		if v == databaseName {
-			return true, nil
-		}
-	}
-	return false, nil
-}
-
-func collectionExists(collectionName string) bool {
-	_, exists := mongoCollectionsList[collectionName]
-	return exists
-}
-
-func closeMongo() {
-	_ = mongoClient.Disconnect(context.Background())
-	mongoClient = nil
-	mongoDb = nil
+func (conn connType) Close() {
+	_ = conn.client.Disconnect(context.Background())
+	conn.client = nil
+	conn.db = nil
 
 }
